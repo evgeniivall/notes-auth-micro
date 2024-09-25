a2655cc26a6346a563881eb98bb5ebb0885a53d5
Evgenii Vall <ewgenii.wall@gmail.com>
2024-09-25 16:04:33 +0200

    Add Handlebars, html-to-text, moment, and Nodemailer dependencies

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const crypto = require("crypto");
 const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
